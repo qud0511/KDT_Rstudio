90858096b63be2b2b7217172747f8b6128984814
qud0511 <qkrqudwnsqkrqudwns0511@gmail.com>
2022-08-19 16:58:49 +0900

    Update app.R

--- R Lesson/My3rdProject/app.R
+++ R Lesson/My3rdProject/app.R
@@ -1,15 +1,36 @@
 library(shiny)
 
+ui <- pageWithSidebar(
+  headerPanel(h1("데이터테이블 예제")),
+  sidebarPanel(
+    checkboxGroupInput("showbars",
+                       "컬럼을 선택해보세요:",
+                       names(diamonds),
+                       selected = names(diamonds)),
+    helpText("오른쪽에서 탭을 선택하면 다른 데이터도 볼 수 있음.")
+  ),
+  
+  mainPanel(
+    tabsetPanel(
+      tabPanel("diamons", dataTableOutput("mytable1")),
+      tabPanel("mtcars", dataTableOutput("mytable2")),
+      tabPanel("iris", dataTableOutput("mytable3"))
+    )
+  )
+)
+
+
 server <- function(input, output){
   output$mytable1 <- renderDataTable({
-    diamonds[, input$showvars, drop = F]
+    diamonds[, input$showbars, drop = F]
   })
   output$mytable2 <- renderTable({
-    iris
-  })
-  output$mytable3 <- renderDataTable({
     mtcars
-  })
+  }, options = list(bSortClasses = T))
+  output$mytable3 <- renderDataTable({
+    iris
+  }, options = list(aLengthMenu = c(5, 30, 50),
+                    iDisplayLength = 5))
 }
 
 shinyApp(ui, server)
